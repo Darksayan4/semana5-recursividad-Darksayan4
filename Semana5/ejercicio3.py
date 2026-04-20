5fed8f21ce4bffad7591c15b57e44980e8ecfc4a
AMuñozMelendez <121844085+Darksayan4@users.noreply.github.com>
2026-04-19 22:46:52 -0400

    Update ejercicio3.py
    Terminado

--- Semana5/ejercicio3.py
+++ Semana5/ejercicio3.py
@@ -4,10 +4,18 @@ Dado un número entero positivo N, calcular su factorial.
 
 Debe implementar una versión iterativa y una recursiva.
 """
+n=int(input("Ingrese un numero positivo: "))
 
 def factorial_ciclo(n):
-    pass
-
+    res=1
+    for i in range (1,n+1)
+        res *= i
+    return res 
 
 def factorial_recursivo(n):
-    pass
+    if n==0||n==1
+        return 1
+    return n * factorial_recursivo(n-1)
+
+print("El factorial con ciclo es: ",factorial_ciclo(n))
+print("El factorial con recursividad es: ",factorial_recursivo(n))
